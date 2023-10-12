3547b02447f8da12fe96f9b4fcac8dd3db4549b5
Devaraj <103442390+devarazz@users.noreply.github.com>
2023-10-12 10:46:27 +0530

    Create sum, difference, product, and quotient.cpp

---
+++ 01. Mathematics/sum, difference, product, and quotient.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+    // Input two numbers
+    double num1, num2;
+    std::cout << "Enter the first number: ";
+    std::cin >> num1;
+    std::cout << "Enter the second number: ";
+    std::cin >> num2;
+
+    // Calculate and display the results
+    double sum = num1 + num2;
+    double difference = num1 - num2;
+    double product = num1 * num2;
+    double quotient = num1 / num2;
+
+    std::cout << "Sum: " << sum << std::endl;
+    std::cout << "Difference: " << difference << std::endl;
+    std::cout << "Product: " << product << std::endl;
+    std::cout << "Quotient: " << quotient << std::endl;
+
+    return 0;
+}
